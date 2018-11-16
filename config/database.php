eda81ceb6d782e46b8cd40306c74acfa28734d9c
Orestis Rodriguez <orestis@orestisrodriguez.com>
2018-11-16 10:32:31 +0100

    still

--- config/database.php
+++ config/database.php
@@ -54,17 +54,14 @@ return [
     'connections' => [
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
-            'charset' => 'utf8',
+            'driver'    => 'mysql',
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
+            'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'prefix'    => '',
         ],
 
     ],
